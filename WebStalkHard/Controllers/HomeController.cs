fe81064871e753568d4906a2f2763d41ea25c6a4
Tiago Almeida <tiago.almeida@w3k.com.br>
2017-08-17 14:26:20 -0300

    Código para pegar tweets do usuário

--- WebStalkHard/Controllers/HomeController.cs
+++ WebStalkHard/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace WebStalkHard.Controllers
 {
@@ -23,5 +28,76 @@ namespace WebStalkHard.Controllers
         {
             return View();
         }
+
+        public ActionResult SignInWithTwitter()
+        {
+            // You need to set your own keys and screen name
+            var oAuthConsumerKey = "qWh8ir2uy6jgMILcgFxRitq6R";
+            var oAuthConsumerSecret = "cF71OBRKJTNhrncTrH9Ei1HKIoFwOWKodd7AbflDhCRzBKDozh";
+            var oAuthUrl = "https://api.twitter.com/oauth2/token";
+            var screenname = "tiaguinhorawr";
+
+            // Do the Authenticate
+            var authHeaderFormat = "Basic {0}";
+
+            var authHeader = string.Format(authHeaderFormat,
+                Convert.ToBase64String(Encoding.UTF8.GetBytes(Uri.EscapeDataString(oAuthConsumerKey) + ":" +
+                Uri.EscapeDataString((oAuthConsumerSecret)))
+            ));
+
+            var postBody = "grant_type=client_credentials";
+
+            HttpWebRequest authRequest = (HttpWebRequest)WebRequest.Create(oAuthUrl);
+            authRequest.Headers.Add("Authorization", authHeader);
+            authRequest.Method = "POST";
+            authRequest.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+            authRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (Stream stream = authRequest.GetRequestStream())
+            {
+                byte[] content = ASCIIEncoding.ASCII.GetBytes(postBody);
+                stream.Write(content, 0, content.Length);
+            }
+
+            authRequest.Headers.Add("Accept-Encoding", "gzip");
+
+            WebResponse authResponse = authRequest.GetResponse();
+            // deserialize into an object
+            TwitAuthenticateResponse twitAuthResponse;
+            using (authResponse)
+            {
+                using (var reader = new StreamReader(authResponse.GetResponseStream()))
+                {
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    var objectText = reader.ReadToEnd();
+                    twitAuthResponse = JsonConvert.DeserializeObject<TwitAuthenticateResponse>(objectText);
+                }
+            }
+
+            // Do the timeline
+            var timelineFormat = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={0}&include_rts=0&exclude_replies=1&count=100";
+            var timelineUrl = string.Format(timelineFormat, screenname);
+            HttpWebRequest timeLineRequest = (HttpWebRequest)WebRequest.Create(timelineUrl);
+            var timelineHeaderFormat = "{0} {1}";
+            timeLineRequest.Headers.Add("Authorization", string.Format(timelineHeaderFormat, twitAuthResponse.token_type, twitAuthResponse.access_token));
+            timeLineRequest.Method = "Get";
+            WebResponse timeLineResponse = timeLineRequest.GetResponse();
+            var timeLineJson = string.Empty;
+            using (timeLineResponse)
+            {
+                using (var reader = new StreamReader(timeLineResponse.GetResponseStream()))
+                {
+                    timeLineJson = reader.ReadToEnd();
+                }
+            }
+
+            return View();
+        }
     }
+}
+
+public class TwitAuthenticateResponse
+{
+    public string token_type { get; set; }
+    public string access_token { get; set; }
 }
