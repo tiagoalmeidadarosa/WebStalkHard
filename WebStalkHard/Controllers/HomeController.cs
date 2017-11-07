c9a6e56bc750342c5e5fc29d0d2cb89b39a20124
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2017-11-07 19:51:53 -0200

    Alterações nas permissões solicitadas; Mostrando apenas primeiro nome na interface do chatbot;

--- WebStalkHard/Controllers/HomeController.cs
+++ WebStalkHard/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace WebStalkHard.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewData["Permissoes"] = "public_profile,email,user_friends,user_about_me,user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_birthday,user_education_history,user_events,user_games_activity,user_hometown,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationships,user_relationship_details,user_religion_politics,user_tagged_places,user_videos,user_website,user_work_history,read_custom_friendlists,read_insights,read_audience_network_insights,read_page_mailboxes,manage_pages,publish_pages,publish_actions,rsvp_event,pages_show_list,pages_manage_cta,pages_manage_instant_articles,ads_read,ads_management,business_management";
+            ViewData["Permissoes"] = "public_profile,email,user_friends,user_about_me,user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_birthday,user_education_history,user_events,user_games_activity,user_hometown,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationships,user_relationship_details,user_religion_politics,user_tagged_places,user_videos,user_website,user_work_history";
             //public_profile,email,user_friends,user_about_me,user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_birthday,user_education_history,user_events,user_games_activity,user_hometown,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationships,user_relationship_details,user_religion_politics,user_tagged_places,user_videos,user_website,user_work_history,read_custom_friendlists,read_insights,read_audience_network_insights,read_page_mailboxes,manage_pages,publish_pages,publish_actions,rsvp_event,pages_show_list,pages_manage_cta,pages_manage_instant_articles,ads_read,ads_management,business_management,pages_messaging,pages_messaging_subscriptions,pages_messaging_payments,pages_messaging_phone_number
 
             return View();
@@ -144,8 +144,8 @@ namespace WebStalkHard.Controllers
                         //client.AppId = ConfigurationManager.AppSettings["appIdFacebook"];
                         //client.AppSecret = ConfigurationManager.AppSettings["appSecretFacebook"];
 
-                        dynamic retorno = client.Get("me?fields=name,picture");
-                        nome = retorno.name;
+                        dynamic retorno = client.Get("me?fields=first_name,picture"); //Pegando primeiro nome, para que ele pergunte o nome inteiro pro bot
+                        nome = retorno.first_name;
                         urlImage = retorno.picture.data.url;
                     }
 
